14e28f8f42438e309b9e2a70b20690772b99c8ee
jugreen22 <jugreen@bu.edu>
2023-11-30 17:33:36 -0500

    rotation

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,11 +4,26 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import android.util.Log
+import edu.stanford,R.drawable.*
 
+private const val TAG = "MainAcitvity"
 class MainActivity : AppCompatActivity() {
+    private lateinit var buttons: List<ImageButtons>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val images = mutableListOf<>()
+
+        buttons = listof(imageButton11, imageButton25, imageButton26, imageButton27, imageButton28, imageButton29
+        imageButton30, imageButton31)
+
+        for (button in buttons){
+            buttons.setClickonListener {
+                Log.i(TAG, "button clicked!")
+        }
+
 
         val easyMode = findViewById<Button>(R.id.Easy_button)
         easyMode.setOnClickListener{
